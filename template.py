31a16badd235df4e68f98326cd7da6f66a9381f1
Vinod Krishnan <vinod.krishnan@ASKPETRARELY.workdayinternal.com>
2021-11-30 14:40:03 -0800

    init

---
+++ template.py
@@ -0,0 +1,14 @@
+def part_2():
+    pass
+
+def part_1():
+    pass
+
+
+def parse(data):
+    pass
+
+with open('inputs.txt', 'r') as fh:
+    data = fh.read().splitlines()
+
+    processed_data = parse(data)
